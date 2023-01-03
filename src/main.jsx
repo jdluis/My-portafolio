78f6cf704e48f74ca58b1d68a2d0483785f2f6eb
jdluis <jdluisdev@gmail.com>
2023-01-03 14:39:39 +0000

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Root from "./routes/root";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
---
+++ src/routes/root.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+import App from "../App";
+
+export default function Root() {
+    return (
+      <>
+        <App />
+      </>
+    );
+  }
