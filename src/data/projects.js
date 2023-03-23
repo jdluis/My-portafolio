e8b1089bf77a4cc718b5de382075b3ed9f34fcfd
jdluis <jdluisdev@gmail.com>
2023-03-23 17:21:59 +0000

    format with prettier

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,8 +1,8 @@
- const projects = [
+const projects = [
   {
     id: '001',
-    type: "Final Project on IronHack",
-    subTitle: "Practice",
+    type: 'Final Project on IronHack',
+    subTitle: 'Practice',
     title: 'Flexy Learn',
     github: {
       client: 'https://github.com/jdluis/flexi-learn-client',
@@ -12,14 +12,13 @@
     description:
       'The online platform that allows you to learn in a flexible and personalized way with courses taught by experts in different areas, from the comfort of your home and at your own pace, so that you can achieve your goals and continue to grow personally and professionally.',
     techStack: ['JSX', 'TailwindCSS', 'React', 'Express', 'MongoDb', 'Mongoose', 'Ayax'],
-    preview_img:
-      '/src/assets/images/logoFlexiLearn.png',
+    preview_img: '/src/assets/images/logoFlexiLearn.png',
   },
 
   {
     id: '002',
-    type: "Project on IronHack",
-    subTitle: "Practice",
+    type: 'Project on IronHack',
+    subTitle: 'Practice',
     title: 'Talent Match',
     github: {
       client: 'https://github.com/jdluis/flexi-learn-client',
@@ -29,13 +28,12 @@
     description:
       'The app is a developer search engine that helps companies find talented developers based on specific criteria such as skill set, experience level, and location. ',
     techStack: ['JSX', 'TailwindCSS', 'React', 'Express', 'MongoDb', 'Mongoose', 'Ayax'],
-    preview_img:
-      '/src/assets/images/logoTalentMatch.png',
+    preview_img: '/src/assets/images/logoTalentMatch.png',
   },
   {
     id: '003',
-    type: "Project on IronHack",
-    subTitle: "Practice",
+    type: 'Project on IronHack',
+    subTitle: 'Practice',
     title: 'The Sun Rises Red',
     github: {
       client: 'https://github.com/jdluis/flexi-learn-client',
@@ -45,9 +43,8 @@
     description:
       'The dark time rise again, the land of the forgotten humanity is in danger one more time, orc are raised again in the middle heath, we must save them. Use your hero to prevent the orcs from approaching the towns and cities. The more we kill, the more lives we save.',
     techStack: ['JSX', 'TailwindCSS', 'React', 'Express', 'MongoDb', 'Mongoose', 'Ayax'],
-    preview_img:
-    '/src/assets/images/theSunRisesRed.png',  },
-
+    preview_img: '/src/assets/images/theSunRisesRed.png',
+  },
 ];
 
-export default projects
+export default projects;
