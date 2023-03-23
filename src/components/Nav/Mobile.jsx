b879eb0fad6c289f60474fd9a0a4c80ba141c003
jdluis <jdluisdev@gmail.com>
2023-03-23 16:53:28 +0000

    nav responsive add

---
+++ src/components/Nav/Mobile.jsx
@@ -0,0 +1,48 @@
+
+import { useState } from "react";
+import { RiAccountCircleLine, RiAncientPavilionLine, RiHealthBookLine, RiLightbulbFlashLine, RiMenuUnfoldLine } from "react-icons/ri"
+import { Link } from "react-router-dom"
+import { AiOutlineClose } from 'react-icons/ai';
+function Mobile() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleOpenMenu = () => {
+    setIsMenuOpen(true);
+}
+
+const handleCloseMenu = () => {
+  setIsMenuOpen(false);
+}
+
+
+  return (
+    <div hidden id="mobile-nav-container">
+        <div className={isMenuOpen ?? 'hidden'}>
+            <button className="btn-open" onClick={handleOpenMenu}><RiMenuUnfoldLine /></button>
+        </div>
+
+        <nav onMouseLeave={handleCloseMenu} className={isMenuOpen ? 'nav-mobile' : 'hidden nav-mobile'}>
+        <button className="btn-close" onClick={handleCloseMenu}><AiOutlineClose /></button>
+          <Link onClick={handleCloseMenu} to='/' className='nav-links'>
+            <RiAncientPavilionLine className='icon-menu' />
+            Home
+          </Link>
+          <Link onClick={handleCloseMenu} to='/about' className='nav-links'>
+            <RiAccountCircleLine className='icon-menu' />
+            About
+          </Link>
+          <Link onClick={handleCloseMenu} to='/works' className='nav-links'>
+            <RiLightbulbFlashLine className='icon-menu' />
+            Works
+          </Link>
+          <Link onClick={handleCloseMenu} to='/contact' className='nav-links'>
+            <RiHealthBookLine className='icon-menu' />
+            Contact
+          </Link>
+        </nav>
+    
+    </div>
+  )
+}
+
+export default Mobile
--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -8,48 +8,52 @@ import {
   RiArrowDownLine,
 } from 'react-icons/ri';
 import './index.css';
+import Mobile from './Mobile';
 
 const iconReact = 'src/assets/icons/react.svg';
 const iconVite = 'src/assets/icons/vitejs.svg';
 
 function Nav() {
-
   //check this handle, doesn't work properly
   const handleGoBottom = () => {
-    window.scrollTo(0, document.body.scrollHeight); 
-  }
-  
+    window.scrollTo(0, document.body.scrollHeight);
+  };
+
   return (
-    <div>
-      <div className='section-container-left'></div>
-      <nav className='nav-container'>
-        <Link to='/' className='nav-links'>
-          <RiAncientPavilionLine className='icon-menu' />
-          Home
-        </Link>
-        <Link to='/about' className='nav-links'>
-          <RiAccountCircleLine className='icon-menu' />
-          About
-        </Link>
-        <Link to='/works' className='nav-links'>
-          <RiLightbulbFlashLine className='icon-menu' />
-          Works
-        </Link>
-        <Link to='/contact' className='nav-links'>
-          <RiHealthBookLine className='icon-menu' />
-          Contact
-        </Link>
-      </nav>
-      <div className='section-container-right'>
-        <div className='img-container'>
-          <img src={iconReact} alt="react icon"/>
-          <img src={iconVite} alt="vite icon"/>
-        </div>
-        <div onClick={handleGoBottom} className='arrow-container '>
-          <RiArrowDownLine  className='arrow' />
+    <>
+      <div id='menu-container'>
+        <div className='section-container-left'></div>
+        <nav className='nav-container'>
+          <Link to='/' className='nav-links'>
+            <RiAncientPavilionLine className='icon-menu' />
+            Home
+          </Link>
+          <Link to='/about' className='nav-links'>
+            <RiAccountCircleLine className='icon-menu' />
+            About
+          </Link>
+          <Link to='/works' className='nav-links'>
+            <RiLightbulbFlashLine className='icon-menu' />
+            Works
+          </Link>
+          <Link to='/contact' className='nav-links'>
+            <RiHealthBookLine className='icon-menu' />
+            Contact
+          </Link>
+        </nav>
+        <div className='section-container-right'>
+          <div className='img-container'>
+            <img src={iconReact} alt='react icon' />
+            <img src={iconVite} alt='vite icon' />
+          </div>
+          <div onClick={handleGoBottom} className='arrow-container '>
+            <RiArrowDownLine className='arrow' />
+          </div>
         </div>
       </div>
-    </div>
+
+      <Mobile />
+    </>
   );
 }
 
