47ae3be504f8f0d6437a21443ddaeb49cf4ff3ac
jdluis <jdluisdev@gmail.com>
2023-05-15 17:22:36 +0100

    about tree completed

--- src/components/timeline/index.jsx
+++ src/components/timeline/index.jsx
@@ -37,10 +37,7 @@ const Timeline = () => {
           <div>
             <time>2019</time>
             <p>
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
-              euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim
-              veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea
-              commodo consequat.
+              I start to consider my current professional future and decide to make a change while continuing to study aerospace mechanics and work. Due to my passion for computers, I decide to investigate the programming field more seriously.
             </p>
           </div>
         </li>
@@ -48,10 +45,10 @@ const Timeline = () => {
           <div>
             <time>2020</time>
             <p>
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
-              euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim
-              veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea
-              commodo consequat.
+              This year, I have already decided to start programming with web development technologies first and I complete a semester of higher vocational training in multiplatform web app development, where I receive a scholarship to enhance my opportunities in the industry.
+            </p>
+            <p>
+              I decide to leave the vocational training and, unfortunately, the Domingo Alonso scholarship as well. One of the reasons was the inability of Education to adapt to new technologies, which were outdated and often resulted in wasting a lot of time. That's why I continue as a self-taught learner.
             </p>
           </div>
         </li>
@@ -59,10 +56,9 @@ const Timeline = () => {
           <div>
             <time>2021</time>
             <p>
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
-              euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim
-              veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea
-              commodo consequat.
+              With a clear idea and fully focused on my new professional career, I dive into the world of web pages, tutorials on JavaScript in Udemy, tutorials on advanced CSS and libraries on YouTube. I begin to connect with digital nomads who are already in the position I aspire to be and they advise me on which path to choose. I discover Tailwind CSS and create applications using vanilla JavaScript.
+
+              This year, I open my GitHub account.
             </p>
           </div>
         </li>
@@ -70,10 +66,10 @@ const Timeline = () => {
           <div>
             <time>2022</time>
             <p>
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
-              euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim
-              veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea
-              commodo consequat.
+              I continued my studies, completing various certifications on platforms like CoderHouse, Platzi, Telefonica, etc., focused on web development for the frontend and agile methodologies.
+            </p>
+            <p>
+              It's the year when I managed to find my first job through LinkedIn (IT Services and IT Consulting) as a web developer, which was a very important personal and professional step for me. In September, I stopped working for that company and decided to keep learning, enrolling in a bootcamp at the beginning of next year.
             </p>
           </div>
         </li>
@@ -81,10 +77,7 @@ const Timeline = () => {
           <div>
             <time>2023</time>
             <p>
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
-              euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim
-              veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea
-              commodo consequat.
+              I'm starting my Bootcamp at Ironhack, specializing in MERN technologies (MongoDB, Express, React, and Node). During the program, I completed a project focused on Object-Oriented Programming (OOP) and two projects focused on FullStack applications, as well as numerous intensive exercises for 2 months.
             </p>
           </div>
         </li>
@@ -92,10 +85,7 @@ const Timeline = () => {
           <div>
             <time>Actualidad</time>
             <p>
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
-              euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim
-              veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea
-              commodo consequat.
+              I am constantly learning new technologies and updating the ones I already know, in addition to creating a project that allows me to learn while practicing. The technologies I am focused on are: TypeScript, Nest.js, Jest, and other testing libraries...
             </p>
           </div>
         </li>
